81157966fcbc4d74d7df9d302d4b4403441caad9
igvan-de <vdsmissenigor@gmail.com>
2022-05-31 21:13:37 +0200

    update detailedBeer page

--- src/components/WhatBeerDetails.vue
+++ src/components/WhatBeerDetails.vue
@@ -11,11 +11,11 @@
                         <img :src="beer.image_url">
                     </div>
                     <div class="detailedBeer_wrapper-item-content">
-                        <h2>Food pairing</h2>
+                        <h2 class="content-header">Description</h2>
                         <p>Description: {{beer.description}}</p>
-                        <hr>
+                        <hr class="detailedBeer_wrapper-item-content-line">
                         <p>Some tips of the brewers themselves: {{beer.brewers_tips}}</p>
-                        <hr>
+                        <hr class="detailedBeer_wrapper-item-content-line">
                         <p>First brewed in: {{beer.first_brewed}}</p>
                     </div>
                 </div>
@@ -24,6 +24,7 @@
                         <img src="../assets/images/hot-temperature.png">
                     </div>
                     <div class="detailedBeer_wrapper-item-content">
+                        <h2 class="content-header">Brew Method</h2>
                         <p>Boil volume: {{beer.boil_volume.value}} {{beer.boil_volume.unit}}</p>
                         <p>Fermentation: {{beer.method.fermentation.temp.value}} {{beer.method.fermentation.temp.unit}}</p>
                         <div v-for="mash of beer.method.mash_temp" :key="mash">
@@ -37,7 +38,7 @@
                         <img src="../assets/images/cutlery.png">
                     </div>
                     <div class="detailedBeer_wrapper-item-content">
-                        <h2>Food pairing</h2>
+                        <h2 class="content-header">Food Pairing</h2>
                         <div v-for="food of beer.food_pairing"
                         :key="food">
                             <li>{{food}}</li>
@@ -52,6 +53,7 @@
                             <img src="../assets/images/hop.png">
                         </div>
                         <div class="detailedBeer_wrapper-item-contentRows">
+                            <h2 class="content-header">Ingredients</h2>
                             <div 
                             v-for="hop of beer.ingredients.hops"
                             :key="hop"
@@ -60,11 +62,12 @@
                                 <ul>Attribute: {{hop.attribute}}</ul>
                                 <ul>When to add: {{hop.add}}</ul>
                                 <ul>Amount: {{hop.amount.value}} {{hop.amount.unit}}</ul>
-                                <hr>
+                                <hr class="detailedBeer_wrapper-item-content-line">
+                                <!-- display only first 3 and if there a re more, add see more option -->
                             </div>
                         </div>
                     </div>
-                    <hr>
+                    <hr class="detailedBeer_wrapper_right-ingredientsRow-line" />
                     <div class="detailedBeer_wrapper_right-ingredientsRow">
                         <div class="detailedBeer_wrapper-item-img">
                             <img src="../assets/images/malt.png">
@@ -76,10 +79,11 @@
                             class="detailedBeer_wrapper-item-content">
                                 <ul>Name: {{malt.name}}</ul>
                                 <ul>Amount: {{malt.amount.value}} {{malt.amount.unit}}</ul>
+                                <hr class="detailedBeer_wrapper-item-content-line">
                             </div>
                         </div>
                     </div>
-                    <hr>
+                    <hr class="detailedBeer_wrapper_right-ingredientsRow-line">
                     <div class="detailedBeer_wrapper_right-ingredientsRow">
                         <div class="detailedBeer_wrapper-item-img">
                             <img src="../assets/images/wheat.png">
@@ -94,6 +98,7 @@
                         <img src="../assets/images/barrel.png">
                     </div>
                     <div class="detailedBeer_wrapper-item-content">
+                        <h2 class="content-header">Extra Information</h2>
                         <ul>ABV: {{beer.abv}}</ul>
                         <ul>Attenuation level: {{beer.attenuation_level}}</ul>
                         <ul>PH: {{beer.ph}}</ul>
@@ -135,6 +140,11 @@ export default({
             gap: 2rem;
         }
 
+            .content-header {
+                font-family: 'Shadow Into Light';
+                font-size: 3rem;
+            }
+
             .detailedBeer-wrapper_left {
                 flex-direction: column;
                 max-width: 25rem;
@@ -144,11 +154,16 @@ export default({
                     display: flex;
                     flex-direction: column;
                     margin-bottom: 3rem;
+                    line-height: 4rem;
                 }
 
                     .detailedBeer-header h2 {
                         font-family: 'Koulen';
-                        font-size: 3rem;
+                        font-size: 6rem;
+                    }
+
+                    .detailedBeer-header p {
+                        font-size: 2rem;
                     }
 
                 .detailedBeer_wrapper-item-img {
@@ -165,6 +180,11 @@ export default({
                     display: flex;
                     flex-direction: column;
                 }
+
+                    .detailedBeer_wrapper-item-content-line {
+                        border: 1px solid var(--vt-c-text-dark-2);
+                        margin: 1rem;
+                    }
 
                 .detailedBeer_wrapper_left-beer {
                     display: flex;
@@ -224,6 +244,7 @@ export default({
                         display: flex;
                         flex-direction: row;
                         align-items: center;
+                        margin: 2rem;
                     }
 
                         .detailedBeer_wrapper-item-contentRows {
@@ -235,6 +256,10 @@ export default({
                                 display: flex;
                                 flex-direction: column;
                             }
+
+                    .detailedBeer_wrapper_right-ingredientsRow-line {
+                        border: 1px solid var(--vt-c-text-dark-3);
+                    }
 
                 .detailedBeer_wrapper_right-extraInfo {
                     display: flex;
