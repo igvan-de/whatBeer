ac700dda06046e528390e5e321c459b70b0b4b67
igvan-de <vdsmissenigor@gmail.com>
2022-06-05 20:56:15 +0200

    working on layout beers page

--- src/components/Beers.vue
+++ src/components/Beers.vue
@@ -1,27 +1,33 @@
 <template>
     <section class="listedBeers_wrapper">
-      <div class="listedBeers_wrapper-beers"
+      <div class="listedBeers_wrapper-beerlist"
       v-for="beer in listedBeers"
       :key="beer.id">
-        <div class="aboutBeer">
-          <h3>{{beer.name}}</h3>
-          <p>{{beer.tagline}}</p>
-        </div>
-        <div class="beerImg">
-          <img :src="beer.image_url">
+        <div class="listedBeers_wrapper-beers">
+          <div class="listedBeers_wrapper-aboutBeer">
+            <h3>{{beer.name}}</h3>
+            <p>{{beer.tagline}}</p>
+          </div>
+          <div class="listedBeers_wrapper-beerImg">
+            <img :src="beer.image_url">
+          </div>
         </div>
       </div>
       <div class="listedBeers_wrapper-pagination">
-        <button
+        <a href="javascript:void(0)"
           v-if="pageNumber > 1"
-          @click="paginationPrevious">
+          @click="paginationPrevious"
+          class="listedBeers_wrapper-pagination-button">
+          <img src="../assets/images/left-arrow-previous.png">
           previous
-        </button>
-        <button
+        </a>
+        <a href="javascript:void(0)"
           v-if="pageNumber < 13"
-          @click="paginationNext">
+          @click="paginationNext"
+          class="listedBeers_wrapper-pagination-button">
           next
-        </button>
+          <img src="../assets/images/right-arrow-next.png">
+        </a>
       </div>
     </section>
 </template>
@@ -63,40 +69,82 @@ export default({
 <style scoped>
 @import '../assets/base.css';
 
-.listedBeers_wrapper-beers {
+.listedBeers_wrapper-beerlist {
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  padding: 1.5rem;
+  flex-direction: column;
+  background-image: url("../assets/images/food_img_ipa.jpeg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 65rem;
+  background-position: 0rem;
+  margin-bottom: 0.2rem;
 }
 
-  .aboutBeer {
+  .listedBeers_wrapper-beers {
     display: flex;
-    flex-direction: column;
-    min-width: 20rem;
-    padding: 2rem;
-  }
-
-  .aboutBeer h3 {
-    font-family: 'Koulen';
-    font-size: 1.5rem;
-  }
-
-  .beerImg {
-    display: flex;
+    flex-direction: row;
+    align-items: center;
     justify-content: flex-end;
+    padding: 5rem;
+    margin-right: 10rem;
+    background-color: var(--vt-c-black-mute-transparant);
+    border-radius: 0 2rem 2rem 0;
+    
   }
 
-  .beerImg img {
-    width: 5rem;
-    height: 15rem;
-  }
+    .listedBeers_wrapper-aboutBeer {
+      display: inherit;
+      flex-direction: column;
+      color: white;
+      min-width: 20rem;
+      padding: 2rem;
+    }
 
-  .listedBeers_wrapper-pagination {
-    display: flex;
-    justify-content: center;
-  }
+    .listedBeers_wrapper-aboutBeer h3 {
+      font-family: 'Koulen';
+      font-size: 3rem;
+      font-weight: bolder;
+    }
+
+    .listedBeers_wrapper-aboutBeer p {
+      font-size: 1.5rem;
+      font-weight: bolder;
+    }
+
+    .listedBeers_wrapper-beerImg {
+      display: flex;
+      justify-content: flex-end;
+    }
+
+    .listedBeers_wrapper-beerImg img {
+      width: 5rem;
+      height: 15rem;
+    }
+
+    .listedBeers_wrapper-pagination {
+      display: flex;
+      justify-content: center;
+      gap: 2rem;
+      margin-bottom: 5rem;
+    }
+
+      .listedBeers_wrapper-pagination a {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        gap: 0.5rem;
+        color: black;
+        font-weight: bold;
+        font-size: 1rem;
+      }
+
+      .listedBeers_wrapper-pagination img {
+        width: 2rem;
+      }
+
+      .listedBeers_wrapper-pagination-button:hover {
+        transform: scale(110%);
+      }
 
 @media (max-width: 1024px) {
 
